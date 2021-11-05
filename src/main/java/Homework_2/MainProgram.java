77ed1c20fafa20d0b47fc9b35738a80c52ec3f65
HenryBuckinson <abdula2341@gmail.com>
2021-11-05 21:15:25 +0300

    Измененил название метода в соответствии с lowerCamelCase.

--- src/main/java/Homework_2/MainProgram.java
+++ src/main/java/Homework_2/MainProgram.java
@@ -3,15 +3,15 @@ package Homework_2;
 public class MainProgram {
 
     public static void main(String[] args) {
-        System.out.println("Площадь круга = "+AreaCircle(4));
-        System.out.println("Плотность тела = "+BodyDensity(3.5,0.238));
+        System.out.println("Площадь круга = "+areaCircle(4));
+        System.out.println("Плотность тела = "+bodyDensity(3.5,0.238));
     }
 
     /**
      * @param radius  радиус круга.
      * @return Результатом метод возвращает значение площади круга, округлённого до двух знаков после запятой.
      */
-    public static double AreaCircle(double radius) {
+    public static double areaCircle(double radius) {
             if (radius > 0) {
                 double scale = Math.pow(10, 2);
                 return Math.ceil(Math.pow(radius, 2) * Math.PI * scale) / scale;
@@ -23,7 +23,7 @@ public class MainProgram {
      * @return Результатом метод возвращает значение плотности тела, округлённого до двух знаков после запятой.
      * Входные параметры необходимо задавать в соответствии с системой СИ.
      */
-    public static double BodyDensity(double mass, double capacity){
+    public static double bodyDensity(double mass, double capacity){
         if(mass>0 && capacity>0){
             double scale = Math.pow(10, 2);
             return Math.ceil(mass/capacity*scale)/scale;
