7f3f46de19c7065eccb455b67df8e216436df647
HenryBuckinson <abdula2341@gmail.com>
2021-11-29 20:10:23 +0300

    коррекция

--- src/main/java/homework_8_firstExercise_alternate/Elephant.java
+++ src/main/java/homework_8_firstExercise_alternate/Elephant.java
@@ -1,6 +1,7 @@
 package homework_8_firstExercise_alternate;
 
 public class Elephant extends Animals{
+    @Override
     public void voice(){
         System.out.println("Слон: -Phhhhoooo");
     }
