ca3fe3770d07bd6e20048f3512592b44c15906e1
HenryBuckinson <abdula2341@gmail.com>
2021-11-30 22:57:05 +0300

    д/з №8 коррекция имени объекта

--- src/main/java/homework_8_firstExercise/MainProgram.java
+++ src/main/java/homework_8_firstExercise/MainProgram.java
@@ -2,8 +2,8 @@ package homework_8_firstExercise;
 
 public class MainProgram {
     public static void main(String[] args) {
-        Animal o = new Animal();
-        o.voice();
+        Animal animals = new Animal();
+        animals.voice();
         Animal[] objArr = new Animal[]{new Cat(), new Dog(), new Cow(), new Elephant()};
         for (Animal i : objArr) {
             i.voice();
