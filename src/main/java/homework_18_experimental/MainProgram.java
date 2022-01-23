83f1fb7df387dd40d4598b3d78f3f6c48fede6fe
HenryBuckinson <abdula2341@gmail.com>
2022-01-23 12:07:19 +0300

    В методе wrappingLines() изменил использование интерфейса Callable на Runnable

--- src/main/java/homework_18_experimental/MainProgram.java
+++ src/main/java/homework_18_experimental/MainProgram.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -16,24 +15,28 @@ public class MainProgram {
         System.out.println();
         wrappingLines();
 
+
     }
 
     @SneakyThrows
     public static void wrappingLines() {
         System.out.println("Распаковка текста файла запущена...");
-        try (BufferedReader reader = new BufferedReader(new FileReader("G:\\Program Files\\IDEA_Projects\\homework-1\\src\\main\\java\\homework_18\\RandomText.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("G:\\Program Files\\IDEA_Projects\\homework-1\\src\\main\\java\\homework_18_experimental\\RandomText.txt"))) {
             String line;
             long count = 1;
             while ((line = reader.readLine()) != null) {
                 ExecutorService executorThread = Executors.newSingleThreadExecutor();
                 String order = "Файл № " + count + ".txt";
-                File file = new File("G:\\Program Files\\IDEA_Projects\\homework-1\\src\\main\\java\\homework_18\\wrappedFiles\\" + order);
+                File file = new File("G:\\Program Files\\IDEA_Projects\\homework-1\\src\\main\\java\\homework_18_experimental\\wrappedFiles\\" + order);
                 final String result = line;
-                Callable<Void> writing = () -> {
-                    FileWriter write = new FileWriter(file);
-                    write.write(result);
-                    write.close();
-                    return null;
+                Runnable writing = new Runnable() {
+                    @SneakyThrows
+                    @Override
+                    public void run() {
+                        FileWriter write = new FileWriter(file);
+                        write.write(result);
+                        write.close();
+                    }
                 };
                 executorThread.submit(writing);
                 executorThread.shutdown();
